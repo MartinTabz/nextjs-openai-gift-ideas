3129a734190ccd153dc5cb6d73318dae5b8ff3b1
Martin Šíl <martin.sil1337@gmail.com>
2023-02-12 12:48:05 +0100

    Komunikace s AI

--- pages/api/getideas.js
+++ pages/api/getideas.js
@@ -1,5 +1,45 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { openai } from '@/utils/openai';
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+	if (req.method === 'POST') {
+		const data = req.body;
+		if (!data) {
+			res.status(400).json({ error: 'Body is missing' });
+			return;
+		}
+		if (!data.gender || !data.age || !data.interests) {
+			res.status(400).json({ error: 'Some data is missing' });
+			return;
+		}
+		var gender = data.gender;
+		if (gender === 'other') {
+			gender = 'male and female';
+		}
+		var intereststring = '';
+		for (var i = 0; i < data.interests.length; i++) {
+			intereststring += data.interests[i];
+			if (i < data.interests.length - 1) {
+				intereststring += ', ';
+			}
+		}
+		var prompt = `Give me 10 gift ideas for ${data.age} year old ${gender}. His interests are ${intereststring}. Every idea should be some kind of product or toy, that sells on site like Amazon.`;
+
+		const airesponse = await openai.createCompletion({
+			model: 'text-davinci-003',
+			prompt: prompt,
+			temperature: 1,
+			max_tokens: 300,
+			top_p: 1,
+			frequency_penalty: 0.1,
+			presence_penalty: 0.1,
+		});
+		console.log(airesponse.data.choices[0]);
+
+		res.status(200).json({
+			message: 'Comunication was succesfull',
+		});
+		return;
+	} else {
+		res.status(405).json({ error: 'Wrong request method!' });
+	}
 }
