938770b06766834a012f893f65e4f067d349816f
Martin Šíl <martin.sil1337@gmail.com>
2023-03-07 01:17:25 +0100

    Přidání inputů a limitování requestů na 5 za minutu na uživatele

--- pages/api/getideas.js
+++ pages/api/getideas.js
@@ -1,62 +1,91 @@
 import { openai } from '@/utils/openai';
+import { Limit } from '@/utils/rate-limit';
+import { v4 as uuidv4 } from 'uuid';
+
+const options = {
+	interval: 60 * 1000, // 60 seconds
+	uniqueTokenPerInterval: 500, // Max 500 users per second
+};
+
+const limiter = Limit(options);
 
 export default async function handler(req, res) {
 	if (req.method === 'POST') {
-		const data = req.body;
-		if (!data) {
-			res.status(400).json({ error: 'Body is missing' });
-			return;
-		}
-		if (!data.gender || !data.age || !data.interests) {
-			res.status(400).json({ error: 'Some data is missing' });
-			return;
-		}
-
-		// If Other gender is chosen
-		var gender = data.gender;
-		if (gender === 'other') {
-			gender = 'male and female';
-		}
-
-		// Interest array to string
-		var intereststring = '';
-		for (var i = 0; i < data.interests.length; i++) {
-			intereststring += data.interests[i];
-			if (i < data.interests.length - 1) {
-				intereststring += ', ';
-			}
-		}
-
-		// Pronoun
-		var pronoun = 'His';
-		if (data.gender == 'male') {
-			pronoun = 'His';
-		} else if (data.gender == 'female') {
-			pronoun = 'Her';
-		} else {
-			pronoun = 'Its';
-		}
-
-		var prompt = `I will give you information about a person and your goal is to come up with 15 ideas for a gift for them. Every single idea should be a physical product, not a membership or a voucher. Use only short keywords of the product - keywords meaning what would you type to google or amazon to find product of it's niche. Write the results as a JSON array. I will also give you the interests of that specific person but it is not necessary to list ideas by their interests just come up with something useful based on their age and gender. Give me 15 gift ideas for ${data.age} year old ${gender}. ${pronoun} interests are ${intereststring}.`;
-		console.log(prompt);
-		
 		try {
-			const aires = await openai.createCompletion({
-				model: 'text-davinci-003',
-				prompt: prompt,
-				temperature: 1,
-				max_tokens: 250,
-				top_p: 1,
-				frequency_penalty: 0.1,
-				presence_penalty: 0.1,
-			});
-			const suggestions = aires.data?.choices?.[0].text;
-			if (suggestions === undefined) {
-				res.status(500).send({ error: 'Failed to get response from OPENAI' });
+			await limiter.check(res, 5, 'CACHE_TOKEN'); // 5 requests per minute
+			const data = req.body;
+			if (!data) {
+				res.status(400).json({ error: 'Body is missing' });
+				return;
 			}
-			res.status(200).send({ result: suggestions });
-		} catch (err) {
-			res.status(500).send({ error: 'Failed to fetch data' });
+			if (!data.gender || !data.age || !data.interests || !data.role || !data.occasion) {
+				res.status(400).json({ error: 'Some data is missing' });
+				return;
+			}
+
+			// If Other gender is chosen
+			var gender = data.gender;
+			if (gender === 'other') {
+				gender = 'male and female';
+			}
+
+			// Interest array to string
+			var intereststring = '';
+			for (var i = 0; i < data.interests.length; i++) {
+				intereststring += data.interests[i];
+				if (i < data.interests.length - 1) {
+					intereststring += ', ';
+				}
+			}
+
+			// Pronoun
+			var pronoun = 'His';
+			if (data.gender == 'male') {
+				pronoun = 'His';
+			} else if (data.gender == 'female') {
+				pronoun = 'Her';
+			} else {
+				pronoun = 'This persons';
+			}
+
+			var occandint = `${pronoun} interests are ${intereststring}`;
+
+			if (data.occupation !== null && data.occupation.trim() !== '' && data.occupation) {
+				occandint = `${pronoun} occupation is ${data.occupation} and their interests are ${intereststring}`;
+			} else {
+				occandint = `${pronoun} interests are ${intereststring}`;
+			}
+
+			var occasion = ` that are appropriate for a ${data.occasion}.`;
+			if(data.occasion === 'other important day') {
+				occasion = ` that are appropriate for a very important day.`;
+			} else if (data.occasion === 'just because') {
+				occasion = ` that are appropriate as an appreciative gifts for their existence.`;
+			}
+
+			var prompt = `I will give you information about a person and your goal is to come up with 15 ideas for a gift. Every single idea should be a physical product or something that you could find on Amazon, avoid membership, vouchers and courses. Return only short keywords of the product, keywords meaning what would you type to google or amazon to find relevant products. Write the results as a JSON array, without backticks only a clean array like this: ["product1", "product2", "product3"]. I will also give you information (gender, age, occupation, interests) about the person that will receive the gift and relation between a person giving and receiving the gift. It is not necessary to list ideas by their interests and occupation just come up with something useful based on their age and gender. It should be mostly gadgets or it can be clothes, but BE really specific and not ordinary. When picking gifts you can look for connections and pick a gift that connects to those product niches. So, give me 15 ideas for ${data.age} year old ${gender}${occasion} The person giving a gift has a relationship with the person receiving the gift of: ${data.role}. ${occandint}. `;
+			console.log(prompt);
+			try {
+				const aires = await openai.createCompletion({
+					model: 'text-davinci-003',
+					prompt: prompt,
+					temperature: 0.5,
+					max_tokens: 256,
+					top_p: 1,
+					frequency_penalty: 0,
+					presence_penalty: 0,
+					best_of: 2,
+				});
+				const suggestions = aires.data?.choices?.[0].text;
+				if (suggestions === undefined) {
+					res.status(500).send({ error: 'Failed to get response from OPENAI' });
+				}
+				res.status(200).send({ result: suggestions, id: uuidv4() });
+			} catch (err) {
+				res.status(500).send({ error: 'Failed to fetch data' });
+			}
+		} catch {
+			res.status(429).json({ error: 'Rate limit exceeded' });
 		}
 	} else {
 		res.status(405).json({ error: 'Wrong request method!' });
--- pages/index.js
+++ pages/index.js
@@ -8,16 +8,29 @@ import { HiX } from 'react-icons/hi';
 export default function Home() {
 	const [isLoading, setIsLoading] = useState(false);
 	const [gotResults, setGotResults] = useState(false);
-	const [ideasValue, setIdeasValue] = useState('');
+	const [gotError, setGotError] = useState(false);
+	const [ideasValue, setIdeasValue] = useState([]);
 
 	// Gender input and selection box
 	const [selectedGender, setSelectedGender] = useState('');
 	const [openGender, setOpenGender] = useState(false);
 	const [genderError, setGenderError] = useState(false);
 
+	// Relation input and selection box
+	const [selectedRole, setSelectedRole] = useState('');
+	const [openRole, setOpenRole] = useState(false);
+	const [roleError, setRoleError] = useState(false);
+
+	// Occasion input and selection box
+	const [selectedOccasion, setSelectedOccasion] = useState('');
+	const [openOccasion, setOpenOccasion] = useState(false);
+	const [occasionError, setOccasionError] = useState(false);
+
 	// Age input
 	const [ageValue, setAgeValue] = useState('');
 	const [ageError, setAgeError] = useState(false);
+
+	const [occupation, setOccupation] = useState('');
 
 	// Declaration of tags array
 	const [tags, setTags] = useState([]);
@@ -62,11 +75,19 @@ export default function Home() {
 			setGenderError(true);
 			inputerror = true;
 		}
+		if (!selectedRole) {
+			setRoleError(true);
+			inputerror = true;
+		}
+		if (!selectedOccasion) {
+			setOccasionError(true);
+			inputerror = true;
+		}
 		if (!ageValue) {
 			setAgeError(true);
 			inputerror = true;
 		}
-		if (tags.length <= 4) {
+		if (tags.length <= 2) {
 			setTagsError(true);
 			inputerror = true;
 		}
@@ -75,7 +96,10 @@ export default function Home() {
 		if (!inputerror) {
 			setIsLoading(true);
 			const body = {
+				occasion: selectedOccasion,
 				gender: selectedGender,
+				role: selectedRole,
+				occupation: occupation,
 				age: ageValue,
 				interests: tags,
 			};
@@ -88,7 +112,12 @@ export default function Home() {
 			});
 			const suggestions = await res.json();
 			console.log(suggestions.result);
-			setIdeasValue(JSON.parse(suggestions.result));
+			if (suggestions.result) {
+				setIdeasValue(JSON.parse(suggestions.result));
+				console.log(suggestions.id);
+			} else {
+				setGotError(true);
+			}
 			setGotResults(true);
 			setIsLoading(false);
 		}
@@ -102,20 +131,143 @@ export default function Home() {
 			{isLoading ? (
 				<Loading />
 			) : gotResults ? (
-				<section> {ideasValue.map((item, index) => (
-					<h2 key={index}>{item}</h2>
-				 ))}</section>
+				gotError ? (
+					<>error</>
+				) : (
+					<section className={styles.generatedsection}>
+						<h1>Here are your ideas:</h1>
+						<div className={styles.ideasgrid}>
+							{' '}
+							{ideasValue.map((item, index) => (
+								<a
+									target="_blank"
+									rel="noreferrer nofollow noopener"
+									className={styles.idealink}
+									href={'https://www.amazon.com/s?k=' + item}
+									key={index}
+								>
+									<div className={styles.idea}>
+										<h2>{item}</h2>
+									</div>
+								</a>
+							))}
+						</div>
+					</section>
+				)
 			) : (
 				<section className={styles.ideapage}>
 					<div className={styles.formarea}>
 						<form onKeyDown={handleKeydownForm} onSubmit={getIdeas}>
 							<div className={styles.innerform}>
+							<div className={styles.inputgroup}>
+									<label>
+										<span className={styles.important}>*</span>What is the occasion?{' '}
+									</label>
+									<div
+										onClick={() => {
+											setOpenOccasion(!openOccasion);
+											setOpenRole(false);
+											setOpenGender(false);
+											setOccasionError(false);
+										}}
+										className={styles.dropdown}
+										style={{
+											border: occasionError && ' 2px solid red',
+											borderRadius: openOccasion ? '10px 10px 0px 0px' : '10px',
+										}}
+									>
+										<span>{selectedOccasion ? selectedOccasion : 'select'}</span>
+										<BiChevronDown size={25} />
+									</div>
+									<ul
+										style={{ display: openOccasion ? 'block' : 'none' }}
+										className={styles.dropdownitems}
+									>
+										<li
+											onClick={() => {
+												setSelectedOccasion('just because');
+												setOpenOccasion(false);
+											}}
+										>
+											Just because
+										</li>
+										<li
+											onClick={() => {
+												setSelectedOccasion('birthday');
+												setOpenOccasion(false);
+											}}
+										>
+											Birthday
+										</li>
+										<li
+											onClick={() => {
+												setSelectedOccasion('christmas');
+												setOpenOccasion(false);
+											}}
+										>
+											Christmas
+										</li>
+										<li
+											onClick={() => {
+												setSelectedOccasion('nameday');
+												setOpenOccasion(false);
+											}}
+										>
+											Nameday
+										</li>
+										<li
+											onClick={() => {
+												setSelectedOccasion('wedding');
+												setOpenOccasion(false);
+											}}
+										>
+											Wedding
+										</li>
+										<li
+											onClick={() => {
+												setSelectedOccasion('anniversary');
+												setOpenOccasion(false);
+											}}
+										>
+											Anniversary
+										</li>
+										<li
+											onClick={() => {
+												setSelectedOccasion('graduation');
+												setOpenOccasion(false);
+											}}
+										>
+											Graduation
+										</li>
+										<li
+											onClick={() => {
+												setSelectedOccasion('pregnancy');
+												setOpenOccasion(false);
+											}}
+										>
+											Pregnancy
+										</li>
+										<li
+											onClick={() => {
+												setSelectedOccasion('other important day');
+												setOpenOccasion(false);
+											}}
+										>
+											Other important day
+										</li>
+									</ul>
+								</div>
 								{/* Gender input */}
 								<div className={styles.inputgroup}>
-									<label>Select the gender of the person </label>
+									<label>
+										<span className={styles.important}>*</span>Select the gender
+										of the person{' '}
+									</label>
 									<div
 										onClick={() => {
 											setOpenGender(!openGender);
+											setOpenRole(false);
+											setOpenOccasion(false);
 											setGenderError(false);
 										}}
 										className={styles.dropdown}
@@ -159,7 +311,124 @@ export default function Home() {
 								</div>
 
 								<div className={styles.inputgroup}>
-									<label>Age of the person</label>
+									<label>
+										<span className={styles.important}>*</span>What is this person for you?{' '}
+									</label>
+									<div
+										onClick={() => {
+											setOpenRole(!openRole);
+											setRoleError(false);
+											setOpenGender(false);
+											setOpenOccasion(false);
+										}}
+										className={styles.dropdown}
+										style={{
+											border: roleError && ' 2px solid red',
+											borderRadius: openRole ? '10px 10px 0px 0px' : '10px',
+										}}
+									>
+										<span>{selectedRole ? selectedRole : 'select'}</span>
+										<BiChevronDown size={25} />
+									</div>
+									<ul
+										style={{ display: openRole ? 'block' : 'none' }}
+										className={styles.dropdownitems}
+									>
+										<li
+											onClick={() => {
+												setSelectedRole('romantic partner');
+												setOpenRole(false);
+											}}
+										>
+											Romantic partner
+										</li>
+										<li
+											onClick={() => {
+												setSelectedRole('colleague');
+												setOpenRole(false);
+											}}
+										>
+											Colleague
+										</li>
+										<li
+											onClick={() => {
+												setSelectedRole('supervisor');
+												setOpenRole(false);
+											}}
+										>
+											Supervisor
+										</li>
+										<li
+											onClick={() => {
+												setSelectedRole('friend');
+												setOpenRole(false);
+											}}
+										>
+											Friend
+										</li>
+										<li
+											onClick={() => {
+												setSelectedRole('sibling');
+												setOpenRole(false);
+											}}
+										>
+											Sibling
+										</li>
+										<li
+											onClick={() => {
+												setSelectedRole('parent');
+												setOpenRole(false);
+											}}
+										>
+											Parent
+										</li>
+										<li
+											onClick={() => {
+												setSelectedRole('grandparent');
+												setOpenRole(false);
+											}}
+										>
+											Grandparent
+										</li>
+										<li
+											onClick={() => {
+												setSelectedRole('sibling of your parent');
+												setOpenRole(false);
+											}}
+										>
+											Sibling of your parent
+										</li>
+										<li
+											onClick={() => {
+												setSelectedRole('offspring');
+												setOpenRole(false);
+											}}
+										>
+											Offspring
+										</li>
+										<li
+											onClick={() => {
+												setSelectedRole('offspring of your sibling or partners sibling');
+												setOpenRole(false);
+											}}
+										>
+											Offspring of your sibling or partners sibling
+										</li>
+										<li
+											onClick={() => {
+												setSelectedRole('cousin');
+												setOpenRole(false);
+											}}
+										>
+											Cousin
+										</li>
+									</ul>
+								</div>
+
+								<div className={styles.inputgroup}>
+									<label>
+										<span className={styles.important}>*</span>Age of the person
+									</label>
 									<div
 										style={{
 											border: ageError && ' 2px solid red',
@@ -181,14 +450,37 @@ export default function Home() {
 								</div>
 
 								<div className={styles.inputgroup}>
+									<label>Occupation</label>
+									<div
+										style={{
+											border: ageError && ' 2px solid red',
+										}}
+										className={styles.numberinput}
+									>
+										<input
+											type="text"
+											min="1"
+											max="120"
+											value={occupation}
+											placeholder="Policeman"
+											onChange={(e) => {
+												setOccupation(e.target.value);
+											}}
+										/>
+									</div>
+								</div>
+
+								<div className={styles.inputgroup}>
 									<label>
+										<span className={styles.important}>*</span>
 										Enter the person&apos;s interests{' '}
 										<span
+											className={styles.require}
 											style={{
 												color: tagsError && 'red',
 											}}
 										>
-											Atleast 5
+											Atleast 3
 										</span>
 									</label>
 									<div
@@ -228,3 +520,4 @@ export default function Home() {
 		</>
 	);
 }
+
