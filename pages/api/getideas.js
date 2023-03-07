baca684080c1dd9a3b415d5d22d8826a96e14240
Martin Šíl <martin.sil1337@gmail.com>
2023-03-07 20:15:10 +0100

    Změna API routy

--- pages/api/getideas.js
+++ pages/api/getideas.js
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 const options = {
 	interval: 60 * 1000, // 60 seconds
-	uniqueTokenPerInterval: 500, // Max 500 users per second
+	uniqueTokenPerInterval: 100, // Max 500 users per second
 };
 
 const limiter = Limit(options);
@@ -63,7 +63,7 @@ export default async function handler(req, res) {
 				occasion = ` that are appropriate as an appreciative gifts for their existence.`;
 			}
 
-			var prompt = `I will give you information about a person and your goal is to come up with 15 ideas for a gift. Every single idea should be a physical product or something that you could find on Amazon, avoid membership, vouchers and courses. Return only short keywords of the product, keywords meaning what would you type to google or amazon to find relevant products. Write the results as a JSON array, without backticks only a clean array like this: ["product1", "product2", "product3"]. I will also give you information (gender, age, occupation, interests) about the person that will receive the gift and relation between a person giving and receiving the gift. It is not necessary to list ideas by their interests and occupation just come up with something useful based on their age and gender. It should be mostly gadgets or it can be clothes, but BE really specific and not ordinary. When picking gifts you can look for connections and pick a gift that connects to those product niches. So, give me 15 ideas for ${data.age} year old ${gender}${occasion} The person giving a gift has a relationship with the person receiving the gift of: ${data.role}. ${occandint}. `;
+			var prompt = `I will give you information about a person and your goal is to come up with 15 ideas for a gift${occasion} Every single idea should be a physical product or something that you could find on Amazon, avoid membership, vouchers and courses. Return only short keywords of the product, keywords meaning what would you type to google or amazon to find relevant products. Write the results as a JSON array, without backticks only a clean array like this: ["product1", "product2", "product3"]. I will also give you information (gender, age, occupation, interests) about the person that will receive the gift and relation between a person giving and receiving the gift. It is not necessary to list ideas by their interests and occupation just come up with something useful based on their age and gender. It should be mostly gadgets or it can be clothes, but BE really specific and not ordinary. When picking gifts you can look for connections and pick a gift that connects to those product niches. So, give me 15 ideas for ${data.age} year old ${gender}. The person giving a gift has a relationship with the person receiving the gift of: ${data.role}. ${occandint}. `;
 			console.log(prompt);
 			try {
 				const aires = await openai.createCompletion({
